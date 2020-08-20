7c029dd0c2bfea6d836f31b9015ba077a871e154
fuadop <fuadolatunji@gmail.com>
2020-08-20 12:38:48 +0100

    Init commit 😀

---
+++ index.js
@@ -0,0 +1,19 @@
+const submitBtn = document.querySelector(".submit-btn");
+const form = document.querySelector(".email");
+
+if(window.innerWidth <= 700) window.location = "./mobile.html";
+else window.location = "./index.html";
+
+form.addEventListener("keydown",()=>{
+    if(form.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+        form.classList.remove("email-input-validated");
+        form.classList.add("email-input");
+        submitBtn.classList.remove("submit-btn-validated");
+        submitBtn.classList.add("submit-btn");
+    }else{
+        form.classList.remove("email-input");
+        form.classList.add("email-input-validated");
+        submitBtn.classList.remove("submit-btn");
+        submitBtn.classList.add("submit-btn-validated");
+    }
+})
